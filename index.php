f0250188e717351347a6985ad0de42b0813591b2
Rigas Papas <rigaspapas@hotmail.com>
2014-09-06 19:36:37 +0300

    Changed architecture and started restyling with LESS

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	if (!isset( $_GET['p'] )) $p = 'home';
-	else
+	else {
 		switch( $_GET['p'] ) {
 			case 'samples':
 				$p = 'samples';
@@ -16,6 +16,7 @@
 				break;
 			default:
 				$p = 'home';
+		}
 	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -24,64 +25,43 @@
 		<title>Στάντζα | ειδικά κοπτικά</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="description" content="Στάντζα , ειδικά κοπτικά" />
-		<link rel="stylesheet" type="text/css" href="./main.css" />
+		<link rel="stylesheet" type="text/css" href="styles/main.css" />
 		<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-		
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-36179329-1']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
-		
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700&subset=latin,greek' rel='stylesheet' type='text/css'>		
 	</head>
 	
 	<body>
 		<div id="left">
 			<div id="logo">
 				<h1>Στάντζα</h1>
-				<div>ειδικά κοπτικά</div>
+				<div id="moto">ειδικά κοπτικά</div>
 			</div>
-			<div id="menu">
-				<ul id="nav">
-					<li <?php if ($p == 'home') echo 'class="selected" ';?>>
-						<a href="?p=home">Αρχική</a>
-					</li>
-					<li <?php if ($p == 'samples') echo 'class="selected" ';?>>
-						<a href="?p=samples">Προϊόντα</a>
-					</li>
-					<li <?php if ($p == 'shop') echo 'class="selected" ';?>>
-						<a href="?p=shop">Ο χώρος</a>
-					</li>
-					<li <?php if ($p == 'clients') echo 'class="selected" ';?>>
-						<a href="?p=clients">Πελατολόγιο</a>
-					</li>
-					<li <?php if ($p == 'contact') echo 'class="selected" ';?>>
-						<a href="?p=contact">Επικοινωνία</a>
-					</li>
-				</ul>
-			</div>
+			<ul id="navigator">
+				<li <?php if ($p == 'home') echo 'class="selected" ';?>>
+					<a href="?p=home">Αρχική</a>
+				</li>
+				<li <?php if ($p == 'samples') echo 'class="selected" ';?>>
+					<a href="?p=samples">Προϊόντα</a>
+				</li>
+				<li <?php if ($p == 'shop') echo 'class="selected" ';?>>
+					<a href="?p=shop">Ο χώρος</a>
+				</li>
+				<li <?php if ($p == 'clients') echo 'class="selected" ';?>>
+					<a href="?p=clients">Πελατολόγιο</a>
+				</li>
+				<li <?php if ($p == 'contact') echo 'class="selected" ';?>>
+					<a href="?p=contact">Επικοινωνία</a>
+				</li>
+			</ul>
 		</div>
-		<div id="right">
+		<div id="main">
 			<div id="banner">
-				<h2>Ρίξτε μια ματιά</h2>
-				<ul id="slide">
-					<?php
-						for ($i=1; $i<=5; $i++) {
-							$t = rand( 2*$i - 1 , 2*$i );
-							echo '<li>';
-							echo '<img alt="shopImage'.$t.'" src="shop/'.$t.'-th2.JPG" /> ';
-							echo '</li>';
-						}
-					?>
-				</ul>
+				<?php
+					$t = rand( 1, 10 );
+					echo '<li>';
+					echo '<img alt="Shop photo" src="shop/'.$t.'.JPG" /> ';
+					echo '</li>';
+				?>
 			</div>
 			<div id="content">
 				<?php
@@ -103,8 +83,21 @@
 			</div>
 		</div>
 		
-		<script type="text/javascript" src="jquery.js"></script>
-		<script type="text/javascript" src="stantza.js"></script>
-		<script type="text/javascript" src="keys.js"></script>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-36179329-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/stantza.js"></script>
+		<script type="text/javascript" src="js/keys.js"></script>
 	</body>
 </html>
