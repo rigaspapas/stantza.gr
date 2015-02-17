b6322a1f914259512c9c6f743ae9edb3b261fe48
rigaspapas <rigaspapas@gmail.com>
2015-02-18 01:55:33 +0200

    Samples are now in Angular logic

--- js/stantza.js
+++ js/stantza.js
@@ -56,9 +56,56 @@ angular.module( 'stantza', ['ngRoute', 'ngAnimate'] )
                 } );
             } );
         };
-
     }
-] );
+] )
+.controller( 'sampleCtrl', function () {
+    this.samples = [
+        {
+            name: 'Κουτιά',
+            samples: [ 'bluebox', 'bluebox-open', 'ccbox1', 'ccbox2', 'kouti' ]
+        },
+        {
+            name: 'Κουτιά παρουσίασης',
+            samples: [ 'box1', 'box2' ]
+        },
+        {
+            name: 'Ειδικές κατασκευές',
+            samples: [ 'books', 'spbox1', 'spbox2', 'box3', 'book', 'bookopen', 'dinersclub', 'dinersclub-back', 'eurobank', 'eurobank-back', 'purplebox', 'purplebox-closed' ]
+        },
+        {
+            name: 'Εργασίες',
+            samples: [ 'exercises' ]
+        },
+        {
+            name: 'Κάλπες',
+            samples: [ 'kalpi', 'kalpes' ]
+        },
+        {
+            name: 'Κλασέρ - Ντοσιέ',
+            samples: [ 'feelgood1', 'feelgood2', 'ntosie1', 'ntosie2', 'ntosie3', 'ntosie4' ]
+        },
+        {
+            name: 'Πόδια στήριξης',
+            samples: [ 'stand' ]
+        },
+        {
+            name: 'Τσάντες',
+            samples: [ 'bag' ]
+        },
+        {
+            name: 'Παζλ',
+            samples: [ 'pazl' ]
+        },
+        {
+            name: 'Καρτολίνες οροφής',
+            samples: [ 'toyotomi' ]
+        },
+        {
+            name: 'Άλλα δείγματα',
+            samples: [ 'cocacola', 'feta-nikos', 'IMG_7723', 'IMG_7727', 'IMG_7728', 'IMG_7729' ]
+        }
+    ];
+} );
 
 /* Preview of pictures function */
 function create_gallery( photo , name ) {
@@ -74,4 +121,3 @@ function create_gallery( photo , name ) {
 function gallery_close() {
     $( '#black-overlay, #outerprev' ).addClass( 'hidden' );
 }
-
