bf13ef562f676b736227a208266b411f7ce24863
rigaspapas <rigaspapas@gmail.com>
2015-02-18 05:48:09 +0200

    Home has now tabs. Samples are fully structured as variables

--- home.php
+++ home.php
@@ -3,110 +3,119 @@
     Στάντζα, εργαστήρι κοπής και διαμόρφωσης προϊόντων γραφικών τεχνών και μεταξοτυπίας
     <i>&rdquo;</i>
 </div>
+<ul class="home-tab-navigation" ng-init="homeTab = 1">
+    <li class="home-tab-selector" ng-class="{selected:homeTab === 1}" ng-click="homeTab = 1">Προϊόντα</li>
+    <li class="home-tab-selector" ng-class="{selected:homeTab === 2}" ng-click="homeTab = 2">Υλικά</li>
+    <li class="home-tab-selector" ng-class="{selected:homeTab === 3}" ng-click="homeTab = 3">Εργασίες</li>
+</ul>
 <div id="home" class="section">
-	<h3>Τα προϊόντα που κατασκευάζουμε</h3>
-	<ul class="column big">
-		<li>Μενού</li>
-		<li>Stands</li>
-		<li>
-			Κουτιά
-				<ul class="sub">
-					<li>παρουσίασης</li>
-					<li>πίτσας</li>
-					<li>burger</li>
-					<li>συσκευασίας</li>
-					<li>με σχάρα,μαγνήτες, κορδόνι, trucks</li>
-					<li>lock bottom</li>
-					<li>pillow box</li>
-				</ul>
-		</li>
-		<li>
-			Καρτολίνες
-			<ul class="sub">
-				<li>επιτραπέζιες</li>
-				<li>επιδαπέδιες</li>
-				<li>οροφής</li>
-			</ul>
-		</li>
-		<li>Πόδια στήριξης</li>
-		<li>Σουπλά - σουβέρ</li>
-	</ul>
-	<ul class="column big">
-		<li>
-			Τσάντες
-			<ul class="sub">
-				<li>με κορδόνια</li>
-				<li>χερούλια</li>
-				<li>τρύπα</li>
-				<li>τυπωμένες ή μη</li>
-			</ul>
-		</li>
-		<li>
-			Ντοσιέ - φόλντερ - κλασέρ
-			<div class="sub">
-				με μηχανισμούς ή αυτοκόλλητα ελάσματα
-			</div>
-		</li>
-		<li>Ημερολόγια
-			<ul class="sub">
-				<li>τοίχου</li>
-				<li>επιτραπέζια</li>
-			</ul>
-		</li>
-		<li>Κορνίζες</li>
-		<li>
-			Ειδικές κατασκευές
-			<ul class="sub">
-				<li>Box παρουσίασης (προϊόντων ή εντύπων)</li>
-				<li>εσωτερική διαμόρφωση βιβλίων</li>
-				<li>παιχνίδια</li>
-				<li>παζλ</li>
-				<li>δειγματολόγια</li>
-				<li>κάλπες</li>
-				<li>μαγνήτες</li>
-			</ul>
-		</li>
-	</ul>
-	
-	<a id="gotosamples" href="?p=samples">Δείτε φωτογραφίες των προϊόντων <span>&raquo;</span></a>
+    <div class="home-tab products" ng-show="homeTab === 1">
+        <h3>Τα προϊόντα που κατασκευάζουμε</h3>
+        <ul class="column big">
+            <li>Μενού</li>
+            <li>Stands</li>
+            <li>
+                Κουτιά
+                    <ul class="sub">
+                        <li>παρουσίασης</li>
+                        <li>πίτσας</li>
+                        <li>burger</li>
+                        <li>συσκευασίας</li>
+                        <li>με σχάρα,μαγνήτες, κορδόνι, trucks</li>
+                        <li>lock bottom</li>
+                        <li>pillow box</li>
+                    </ul>
+            </li>
+            <li>
+                Καρτολίνες
+                <ul class="sub">
+                    <li>επιτραπέζιες</li>
+                    <li>επιδαπέδιες</li>
+                    <li>οροφής</li>
+                </ul>
+            </li>
+            <li>Πόδια στήριξης</li>
+            <li>Σουπλά - σουβέρ</li>
+            <li>
+                Τσάντες
+                <ul class="sub">
+                    <li>με κορδόνια</li>
+                    <li>χερούλια</li>
+                    <li>τρύπα</li>
+                    <li>τυπωμένες ή μη</li>
+                </ul>
+            </li>
+            <li>
+                Ντοσιέ - φόλντερ - κλασέρ
+                <div class="sub">
+                    με μηχανισμούς ή αυτοκόλλητα ελάσματα
+                </div>
+            </li>
+            <li>Ημερολόγια
+                <ul class="sub">
+                    <li>τοίχου</li>
+                    <li>επιτραπέζια</li>
+                </ul>
+            </li>
+            <li>Κορνίζες</li>
+            <li>
+                Ειδικές κατασκευές
+                <ul class="sub">
+                    <li>Box παρουσίασης (προϊόντων ή εντύπων)</li>
+                    <li>εσωτερική διαμόρφωση βιβλίων</li>
+                    <li>παιχνίδια</li>
+                    <li>παζλ</li>
+                    <li>δειγματολόγια</li>
+                    <li>κάλπες</li>
+                    <li>μαγνήτες</li>
+                </ul>
+            </li>
+        </ul>
 
-	<h3>Τι υλικά χρησιμοποιούμε</h3>
-	<ul class="sub big">
-		<li>μαγνήτες (φύλλο ή μεταλλικοί) </li>
-		<li>πριτσίνια</li>
-		<li>τρουξ</li>
-		<li>βίδες</li>
-		<li>λάστιχο</li>
-		<li>PVC</li>
-		<li>PPC</li>
-		<li>μικροβέλε (Microwelle)</li>
-		<li>βέλκρον (velcron)</li>
-		<li>ειδικά χαρτιά</li>
-		<li>kappa fix</li>
-		<li>μακετόχαρτο</li>
-		<li>τεχνολέν</li>
-		<li>eva</li>
-		<li>υφάσματα</li>
-	</ul>
+        <a id="gotosamples" href="#samples">Δείτε φωτογραφίες των προϊόντων <span>&raquo;</span></a>
 
-	<h3>Εργασίες</h3>
-	<ul class="sub big">
-		<li>λαμινάρισμα επιφανειών</li>
-		<li>κοπή</li>
-		<li>κολλήματα εφαρμογών</li>
-		<li>διάτρηση</li>
-		<li>περφορέ</li>
-		<li>πίκμανση</li>
-		<li>γκοφρέ</li>
-	</ul>
+    </div>
+    <div class="home-tab materials" ng-show="homeTab === 2">
+        <h3>Τι υλικά χρησιμοποιούμε</h3>
+        <ul class="sub big">
+            <li>μαγνήτες (φύλλο ή μεταλλικοί) </li>
+            <li>πριτσίνια</li>
+            <li>τρουξ</li>
+            <li>βίδες</li>
+            <li>λάστιχο</li>
+            <li>PVC</li>
+            <li>PPC</li>
+            <li>μικροβέλε (Microwelle)</li>
+            <li>βέλκρον (velcron)</li>
+            <li>ειδικά χαρτιά</li>
+            <li>kappa fix</li>
+            <li>μακετόχαρτο</li>
+            <li>τεχνολέν</li>
+            <li>eva</li>
+            <li>υφάσματα</li>
+        </ul>
+    </div>
+    
+    <div class="home-tab operations" ng-show="homeTab === 3">
+        <h3>Οι εργασίες που αναλαμβάνουμε</h3>
+        <ul class="sub big">
+            <li>λαμινάρισμα επιφανειών</li>
+            <li>κοπή</li>
+            <li>κολλήματα εφαρμογών</li>
+            <li>διάτρηση</li>
+            <li>περφορέ</li>
+            <li>πίκμανση</li>
+            <li>γκοφρέ</li>
+        </ul>
 
-	<h3>Εργασίες εξωτερικών συνεργατών</h3>
-	<ul class="sub big">
-		<li>εκτύπωση ψηφιακή/λιθογραφική</li>
-		<li>μεταξοτυπίες</li>
-		<li>χρυσοτυπίες</li>
-		<li>συρταροκολλητική</li>
-		<li>σπιράλ</li>
-		<li>κατασκευές πλέξιγκλας</li>
-	</ul>
+        <h3>Εργασίες εξωτερικών συνεργατών</h3>
+        <ul class="sub big">
+            <li>εκτύπωση ψηφιακή/λιθογραφική</li>
+            <li>μεταξοτυπίες</li>
+            <li>χρυσοτυπίες</li>
+            <li>συρταροκολλητική</li>
+            <li>σπιράλ</li>
+            <li>κατασκευές πλέξιγκλας</li>
+        </ul>
+    </div>
 </div>
